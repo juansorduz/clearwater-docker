b5c64def7120b14eb56e35ce240f317c7028f972
juansorduz <juanorduz@unicauca.edu.co>
2018-11-06 09:30:10 -0500

    Update Start script with config env created

--- ScriptsKU/KUStart.sh
+++ ScriptsKU/KUStart.sh
@@ -2,7 +2,8 @@
 users=${1:-0}
 duration=${2:-0}
 cd
-kubectl create configmap env-vars --from-literal=ZONE=default.svc.cluster.local && kubectl apply -f clearwater-docker/kubernetes
+kubectl create configmap env-vars --from-literal=ZONE=default.svc.cluster.local
+kubectl apply -f clearwater-docker/kubernetes
 sleep 120
 cassandrapod=$(kubectl get pods | grep cassandra | cut -d ' ' -f1)
 kubectl exec $cassandrapod /usr/share/clearwater/crest-prov/src/metaswitch/crest/tools/stress_provision.sh 1000
