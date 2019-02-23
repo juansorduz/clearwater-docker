1a148cb97924e99eef442165f7340d3cd11ebe86
juansorduz <juanorduz@unicauca.edu.co>
2019-02-23 09:32:10 -0500

    Updating kubernetes start deployment scripts

--- ScriptsKU/KUStart1.sh
+++ ScriptsKU/KUStart1.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-sudo kubeadm reset
+password=${1:-0}
+echo $password | sudo -S kubeadm reset -f
 rm -rf ~/.kube/
-sudo kubeadm init --pod-network-cidr=192.168.0.0/16
+echo $password | sudo -S kubeadm init --pod-network-cidr=192.168.0.0/16
 mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
+echo $password | sudo -S cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+echo $password | sudo -S chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl taint nodes --all node-role.kubernetes.io/master-
 kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
 kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml
