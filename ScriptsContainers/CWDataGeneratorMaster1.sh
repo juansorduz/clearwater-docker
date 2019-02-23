3ce12e5733f453f112d8858f5c619ee0ab254082
juansorduz <juanorduz@unicauca.edu.co>
2019-02-23 11:30:36 -0500

    Updating start deployment scripts

--- ScriptsContainers/CWDataGeneratorMaster1.sh
+++ ScriptsContainers/CWDataGeneratorMaster1.sh
@@ -2,7 +2,7 @@
 password=${1:-0}
 echo $password | sudo -S kubeadm reset -f
 rm -rf ~/.kube/
-. ~/clearwater-docker/ScriptsCW/CWImagesCreate.sh
+#. ~/clearwater-docker/ScriptsCW/CWImagesCreate.sh
 . ~/clearwater-docker/ScriptsCW/CWCreate1.sh
 . ~/clearwater-docker/ScriptsContainers/CWPruebaContainer1.sh 200 1
 . ~/clearwater-docker/ScriptsContainers/CWPruebaContainer1.sh 500 1
