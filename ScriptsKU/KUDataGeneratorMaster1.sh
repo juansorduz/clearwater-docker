d0b435d55243a894fc6ea3f17c77a05a9b2c705c
juansorduz <juanorduz@unicauca.edu.co>
2019-02-23 10:15:21 -0500

    Updating start deployment scripts

--- ScriptsKU/KUDataGeneratorMaster1.sh
+++ ScriptsKU/KUDataGeneratorMaster1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 password=${1:-0}
 #. ~/clearwater-docker/ScriptsCW/CWCreate1.sh
-. ~/clearwater-docker/ScriptsKU/KuStart1.sh $password
+. ~/clearwater-docker/ScriptsKU/KUStart1.sh $password
 . ~/clearwater-docker/ScriptsKU/KUMonitor1.sh 200 1
 . ~/clearwater-docker/ScriptsKU/KUMonitor1.sh 500 1
 . ~/clearwater-docker/ScriptsKU/KUMonitor1.sh 1000 1
--- ScriptsKU/KUDataGeneratorMaster2.sh
+++ ScriptsKU/KUDataGeneratorMaster2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 password=${1:-0}
 #. ~/clearwater-docker/ScriptsKU/KUCreate2.sh
-. ~/clearwater-docker/ScriptsKU/KuStart2.sh $password 
+. ~/clearwater-docker/ScriptsKU/KUStart2.sh $password 
 . ~/clearwater-docker/ScriptsKU/KUMonitor2.sh 200 1
 . ~/clearwater-docker/ScriptsKU/KUMonitor2.sh 500 1
 . ~/clearwater-docker/ScriptsKU/KUMonitor2.sh 1000 1
