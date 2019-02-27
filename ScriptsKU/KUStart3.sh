53e613a0727cbbb9373a57dda230c7076591d7d2
juansorduz <juanorduz@unicauca.edu.co>
2019-02-27 17:47:17 -0500

    deleting duplicate folders

---
+++ ScriptsKU/KUStart3.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+password=${1:-0}
+echo $password | sudo -S kubeadm reset -f
+rm -rf ~/.kube/
+echo $password | sudo -S kubeadm init --pod-network-cidr=192.168.0.0/16
+mkdir -p $HOME/.kube
+echo $password | sudo -S cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+echo $password | sudo -S chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl taint nodes --all node-role.kubernetes.io/master-
+kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
+kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml
+sleep 5
+cd
+cd clearwater-docker
+kubectl create configmap env-vars --from-literal=ZONE=default.svc.cluster.local
+kubectl apply -f kubernetes3
+sleep 300
+urcassandrapod=$(kubectl get pods | grep cassandra | cut -d ' ' -f1)
+kubectl exec $urcassandrapod /usr/share/clearwater/crest-prov/src/metaswitch/crest/tools/stress_provision.sh 11000
