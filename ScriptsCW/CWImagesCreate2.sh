ccc67f34a649b224218f916475304435f091070f
juansorduz <juanorduz@unicauca.edu.co>
2019-02-27 14:22:18 -0500

    Adding images create script

--- ScriptsCW/CWImagesCreate2.sh
+++ ScriptsCW/CWImagesCreate2.sh
@@ -6,4 +6,4 @@ cd ~/clearwater-docker
 #docker rmi $(docker images -aq)
 #docker network rm $(docker network ls)
 #for i in base astaire cassandra chronos bono ellis homer homestead homestead-prov ralf sprout sipptest sprout-full; do docker build -t clearwater/$i $i ; done
-for i in astaire2 URcassandra MSCcassandra chronos2 bono2 ellis2 homer2 URhomestead MSChomestead homestead-prov2 ralf2 URsprout MSCsprout sipptest; do docker build -t clearwater/$i $i ; done
+for i in base2 astaire2 urcassandra msccassandra chronos2 bono2 ellis2 homer2 urhomestead mschomestead homestead-prov2 ralf2 ursprout mscsprout sipptest; do docker build -t clearwater/$i $i ; done
