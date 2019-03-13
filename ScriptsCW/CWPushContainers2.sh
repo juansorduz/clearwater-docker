ba750ec7f1a91f736e6f93c6224f583041b3b827
juansorduz <juanorduz@unicauca.edu.co>
2019-03-13 17:57:09 -0500

    Updating chronos files

--- ScriptsCW/CWPushContainers2.sh
+++ ScriptsCW/CWPushContainers2.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-nameRepository=${1:-juansorduz}
-
-echo $nameRepository
+option=${1:-0}
+if [ "$option" -eq '1' ]
+then
 nameRepository1=gabrielorozco20
 for i in bono2 homer2 ellis2 urcassandra mschomestead ursprout sipptest
 do
     docker tag clearwater/$i:latest $nameRepository1/clearwater-$i:latest
     docker push $nameRepository1/clearwater-$i:latest
 done
+fi
 
+
+if [ "$option" -eq '2' ]
+then
 nameRepository2=juansorduz
 for i in astaire2 ralf2 chronos2 homestead-prov2 msccassandra urhomestead mscsprout
 do
     docker tag clearwater/$i:latest $nameRepository2/clearwater-$i:latest
     docker push $nameRepository2/clearwater-$i:latest
 done
+fi
