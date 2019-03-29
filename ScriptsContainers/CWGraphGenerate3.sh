8e4c25ae88c5db9d1d3506696d3c8d5a251b1999
GabrielOrozco <GabrielOrozco>
2019-03-29 14:52:30 -0500

    hola

--- ScriptsContainers/CWGraphGenerate3.sh
+++ ScriptsContainers/CWGraphGenerate3.sh
@@ -8,17 +8,17 @@ sed -i '1i urcassandra           urcassandra           urcassandra' $testfolder/
 sed -i '1i msccassandra           msccassandra           msccassandra' $testfolder/msccassandra.csv
 sed -i '1i chronos           chronos           chronos' $testfolder/chronos.csv
 sed -i '1i bono           bono           bono' $testfolder/bono.csv
-#sed -i '1i ellis           ellis           ellis' $testfolder/ellis.csv
+sed -i '1i ellis           ellis           ellis' $testfolder/ellis.csv
 sed -i '1i homer           homer           homer' $testfolder/homer.csv
 sed -i '1i urhomestead           urhomestead           urhomestead' $testfolder/urhomestead.csv
 sed -i '1i mschomestead           mschomestead           mschomestead' $testfolder/mschomestead.csv
-#sed -i '1i homestead-prov           homestead-prov           homestead-prov' $testfolder/homestead-prov.csv
+sed -i '1i homestead-prov           homestead-prov           homestead-prov' $testfolder/homestead-prov.csv
 sed -i '1i ralf           ralf           ralf' $testfolder/ralf.csv
 sed -i '1i ursprout          ursprout           ursprout' $testfolder/ursprout.csv
 sed -i '1i mscsprout          mscsprout           mscsprout' $testfolder/mscsprout.csv
 sed -i '1i sipptest          sipptest           sipptest' $testfolder/sipptest.csv
 #for FILE in astaire cassandra chronos bono ellis homer homestead homestead-prov ralf s_p_r_o_u_t_3 sprout sipptest ; do
-for FILE in astaire urcassandra msccassandra chronos bono homer urhomestead mschomestead ralf ursprout mscsprout sipptest; do
+for FILE in astaire urcassandra msccassandra chronos homestead-prov ellis bono homer urhomestead mschomestead ralf ursprout mscsprout sipptest; do
     gnuplot <<- EOF
         set xlabel "Time in seconds"
         set ylabel "CPU Percentage"
@@ -38,7 +38,7 @@ gnuplot <<- EOF
     set terminal pdf
     set format x "%8.6f"
     set output "$testfolder/Total.pdf"
-    plot "$testfolder/mscsprout.csv" using 0:2 with lines lt rgb "#5F9EA0", "$testfolder/ursprout.csv" using 0:2 with lines lt rgb "#00FFFF", "$testfolder/urcassandra.csv" using 0:2 with lines lt rgb "#8A2BE2", "$testfolder/msccassandra.csv" using 0:2 with lines lt rgb "#FED700", "$testfolder/urhomestead.csv" using 0:2 with lines lt rgb "#8B2BE5", "$testfolder/mschomestead.csv" using 0:2 with lines lt rgb "#FFD705", "$testfolder/bono.csv" using 0:2 with lines lt rgb "#006400"
+    plot "$testfolder/mscsprout.csv" using 0:2 with lines lt rgb "#5F9EA0", "$testfolder/ursprout.csv" using 0:2 with lines lt rgb "#00FFFF", "$testfolder/urcassandra.csv" using 0:2 with lines lt rgb "#8A2BE2", "$testfolder/msccassandra.csv" using 0:2 with lines lt rgb "#FED700", "$testfolder/urhomestead.csv" using 0:2 with lines lt rgb "#F20202", "$testfolder/mschomestead.csv" using 0:2 with lines lt rgb "#FF6100", "$testfolder/bono.csv" using 0:2 with lines lt rgb "#006400"
 EOF
 
 gnuplot <<- EOF
