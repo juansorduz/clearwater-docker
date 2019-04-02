21d1dcdaff7e8fcd8a4378952a4fe802961aa602
juansorduz <GabrielOrozco>
2019-04-02 12:02:47 -0500

    Updating Kubernetes test scripts

--- Scripts/auto_test.sh
+++ Scripts/auto_test.sh
@@ -28,3 +28,4 @@ mkdir -p ~/ClearwaterTestResults/Kubernetes3/$cps$duration
 testfolder=~/ClearwaterTestResults/Kubernetes3/$cps$duration
 touch $testfolder/logsSIPpTest.txt
 kubectl exec $sipptest ./usr/share/clearwater/bin/sip-stress > $testfolder/logsSIPpTest.txt
+kubectl cp $sipptest:/var/log/clearwater-sipp/ $testfolder
