4089cebfdfb6b83df31e92a2b9f7c71c78d5737b
juansorduz <GabrielOrozco>
2019-04-11 16:06:46 -0500

    Updating Local VM Monitor

--- ScriptsKU/KUMonitorDocker.sh
+++ ScriptsKU/KUMonitorDocker.sh
@@ -10,21 +10,21 @@ NumberTest=${5:-2}
 
 #Seding monitor files to local VMs
 source ~/clearwater-docker/Scripts/AddressVM
-echo Copying files to local VMs
-echo AdressVM1: $AddressVM1
-sshpass -p $password scp -r -o StrictHostKeyChecking=no ~/clearwater-docker/Scripts/LocalDockerMonitor.sh worker1@$AddressVM1:~/
-echo AdressVM2: $AddressVM2
-sshpass -p $password scp -r -o StrictHostKeyChecking=no ~/clearwater-docker/Scripts/LocalDockerMonitor.sh worker2@$AddressVM2:~/
-echo AdressVM3: $AddressVM3
-sshpass -p $password scp -r -o StrictHostKeyChecking=no ~/clearwater-docker/Scripts/LocalDockerMonitor.sh worker3@$AddressVM3:~/
-echo AdressVM4: $AddressVM4
-sshpass -p $password scp -r -o StrictHostKeyChecking=no ~/clearwater-docker/Scripts/LocalDockerMonitor.sh worker4@$AddressVM4:~/
-echo AdressVM5: $AddressVM5
-sshpass -p $password scp -r -o StrictHostKeyChecking=no ~/clearwater-docker/Scripts/LocalDockerMonitor.sh worker5@$AddressVM5:~/
-echo AdressVM6: $AddressVM6
-sshpass -p $password scp -r -o StrictHostKeyChecking=no ~/clearwater-docker/Scripts/LocalDockerMonitor.sh worker6@$AddressVM6:~/
-echo AdressVM7: $AddressVM7
-sshpass -p $password scp -r -o StrictHostKeyChecking=no ~/clearwater-docker/Scripts/LocalDockerMonitor.sh worker7@$AddressVM7:~/
+# echo Copying files to local VMs
+# echo AdressVM1: $AddressVM1
+# sshpass -p $password scp -r -o StrictHostKeyChecking=no ~/clearwater-docker/Scripts/LocalDockerMonitor.sh worker1@$AddressVM1:~/
+# echo AdressVM2: $AddressVM2
+# sshpass -p $password scp -r -o StrictHostKeyChecking=no ~/clearwater-docker/Scripts/LocalDockerMonitor.sh worker2@$AddressVM2:~/
+# echo AdressVM3: $AddressVM3
+# sshpass -p $password scp -r -o StrictHostKeyChecking=no ~/clearwater-docker/Scripts/LocalDockerMonitor.sh worker3@$AddressVM3:~/
+# echo AdressVM4: $AddressVM4
+# sshpass -p $password scp -r -o StrictHostKeyChecking=no ~/clearwater-docker/Scripts/LocalDockerMonitor.sh worker4@$AddressVM4:~/
+# echo AdressVM5: $AddressVM5
+# sshpass -p $password scp -r -o StrictHostKeyChecking=no ~/clearwater-docker/Scripts/LocalDockerMonitor.sh worker5@$AddressVM5:~/
+# echo AdressVM6: $AddressVM6
+# sshpass -p $password scp -r -o StrictHostKeyChecking=no ~/clearwater-docker/Scripts/LocalDockerMonitor.sh worker6@$AddressVM6:~/
+# echo AdressVM7: $AddressVM7
+# sshpass -p $password scp -r -o StrictHostKeyChecking=no ~/clearwater-docker/Scripts/LocalDockerMonitor.sh worker7@$AddressVM7:~/
 
 
 #Principal Script to mak3 32 t3sts
