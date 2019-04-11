3acbf050e1664b59588523b3c6aaccb099da78d7
juansorduz <GabrielOrozco>
2019-04-11 15:58:29 -0500

    Updating Local VM Monitor

--- ScriptsKU/KUMonitorDocker.sh
+++ ScriptsKU/KUMonitorDocker.sh
@@ -77,13 +77,22 @@ sleep 2
 ################################################################################
 #Execute monitor VM scripts on background
 ################################################################################
-sshpass -p $password ssh -f -t -o StrictHostKeyChecking=no worker1@$AddressVM1 ". ~/LocalDockerMonitor.sh $cps $duration $NumTest"
-sshpass -p $password ssh -f -t -o StrictHostKeyChecking=no worker2@$AddressVM2 ". ~/LocalDockerMonitor.sh $cps $duration $NumTest"
-sshpass -p $password ssh -f -t -o StrictHostKeyChecking=no worker3@$AddressVM3 ". ~/LocalDockerMonitor.sh $cps $duration $NumTest"
-sshpass -p $password ssh -f -t -o StrictHostKeyChecking=no worker4@$AddressVM4 ". ~/LocalDockerMonitor.sh $cps $duration $NumTest"
-sshpass -p $password ssh -f -t -o StrictHostKeyChecking=no worker5@$AddressVM5 ". ~/LocalDockerMonitor.sh $cps $duration $NumTest"
-sshpass -p $password ssh -f -t -o StrictHostKeyChecking=no worker6@$AddressVM6 ". ~/LocalDockerMonitor.sh $cps $duration $NumTest"
-sshpass -p $password ssh -f -t -o StrictHostKeyChecking=no worker7@$AddressVM7 ". ~/LocalDockerMonitor.sh $cps $duration $NumTest"
+# sshpass -p $password ssh -f -t -o StrictHostKeyChecking=no worker1@$AddressVM1 ". ~/LocalDockerMonitor.sh $cps $duration $NumTest"
+# sshpass -p $password ssh -f -t -o StrictHostKeyChecking=no worker2@$AddressVM2 ". ~/LocalDockerMonitor.sh $cps $duration $NumTest"
+# sshpass -p $password ssh -f -t -o StrictHostKeyChecking=no worker3@$AddressVM3 ". ~/LocalDockerMonitor.sh $cps $duration $NumTest"
+# sshpass -p $password ssh -f -t -o StrictHostKeyChecking=no worker4@$AddressVM4 ". ~/LocalDockerMonitor.sh $cps $duration $NumTest"
+# sshpass -p $password ssh -f -t -o StrictHostKeyChecking=no worker5@$AddressVM5 ". ~/LocalDockerMonitor.sh $cps $duration $NumTest"
+# sshpass -p $password ssh -f -t -o StrictHostKeyChecking=no worker6@$AddressVM6 ". ~/LocalDockerMonitor.sh $cps $duration $NumTest"
+# sshpass -p $password ssh -f -t -o StrictHostKeyChecking=no worker7@$AddressVM7 ". ~/LocalDockerMonitor.sh $cps $duration $NumTest"
+
+sshpass -p $password ssh -f -t -o StrictHostKeyChecking=no worker1@$AddressVM1 "nohup bash . ~/LocalDockerMonitor.sh $cps $duration $NumTest"
+sshpass -p $password ssh -f -t -o StrictHostKeyChecking=no worker2@$AddressVM2 "nohup bash . ~/LocalDockerMonitor.sh $cps $duration $NumTest"
+sshpass -p $password ssh -f -t -o StrictHostKeyChecking=no worker3@$AddressVM3 "nohup bash . ~/LocalDockerMonitor.sh $cps $duration $NumTest"
+sshpass -p $password ssh -f -t -o StrictHostKeyChecking=no worker4@$AddressVM4 "nohup bash . ~/LocalDockerMonitor.sh $cps $duration $NumTest"
+sshpass -p $password ssh -f -t -o StrictHostKeyChecking=no worker5@$AddressVM5 "nohup bash . ~/LocalDockerMonitor.sh $cps $duration $NumTest"
+sshpass -p $password ssh -f -t -o StrictHostKeyChecking=no worker6@$AddressVM6 "nohup bash . ~/LocalDockerMonitor.sh $cps $duration $NumTest"
+sshpass -p $password ssh -f -t -o StrictHostKeyChecking=no worker7@$AddressVM7 "nohup bash . ~/LocalDockerMonitor.sh $cps $duration $NumTest"
+
 
 ################################################################################
 #Deleting old files
