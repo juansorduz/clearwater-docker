3c5873259f1fd0293140d3d96cf82df546a50d65
juansorduz <juanorduz@unicauca.edu.co>
2019-04-15 10:26:07 -0500

    Updating Summary and Promedy scripts

--- ScriptsDat/DatVMDataSummary1.sh
+++ ScriptsDat/DatVMDataSummary1.sh
@@ -45,9 +45,9 @@ while [ $NumTest -lt $NumberTest ]; do
     NumData=0
     while IFS=" " read -r CPU remainder
     do
-      CPU=echo ${CPU%.*}
+      CPU=$(echo ${CPU%.*})
       #echo  $datetime $cpu $ram
-      SumCPU=`echo $SumCPU + $cpu | bc`
+      SumCPU=`echo $SumCPU + $CPU | bc`
       #echo $SumCPU
       let NumData=NumData+1
     done < "$testfolder/$NumTest/$i"
