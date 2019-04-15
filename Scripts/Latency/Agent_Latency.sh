4128c0b9843a26ebbcd6cb10dce2de47f8904362
juansorduz <GabrielOrozco>
2019-04-15 16:57:59 -0500

    Updating

---
+++ Scripts/Latency/Agent_Latency.sh
@@ -0,0 +1,3 @@
+# bin bash
+prueba=${1:-registry}
+/usr/share/clearwater/bin/sipp -i 10.244.1.55 -sf /var/log/clearwater-sipp/sip-stress-$prueba-latency.xml 10.244.1.254:5060 -t tn -s default.svc.cluster.local -inf /usr/share/clearwater/sip-stress/user_test_latency.csv -users 1 -m 1 -default_behaviors all,-bye -max_socket 65000 -trace_stat -trace_rtt -trace_counts -trace_err -max_reconnect -1 -reconnect_sleep 0 -reconnect_close 0 -send_timeout 4000 -recv_timeout 12000 -nostdin
--- Scripts/Latency/Tester_Latency.sh
+++ Scripts/Latency/Tester_Latency.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-local_ip=${1:-127.0.0.0}
+
 directory=~/clearwater-docker/Scripts/Latency
 sipptest=$(kubectl get pods | grep sipptest | cut -d ' ' -f1)
 kubectl cp $directory/sip-stress-registry-latency.xml $sipptest:/usr/share/clearwater/sip-stress/
 kubectl cp $directory/sip-stress-call-latency.xml $sipptest:/usr/share/clearwater/sip-stress/
 kubectl cp $directory/sip-stress-bye-latency.xml $sipptest:/usr/share/clearwater/sip-stress/
 kubectl cp $directory/user_test_latency.sh $sipptest:/usr/share/clearwater/sip-stress/
-
+kubectl cp $directory/Agent_Latency.sh $sipptest:/usr/share/clearwater/sip-stress/
 START=$(date +%s.%N)
-kubectl exec $sipptest "/usr/share/clearwater/bin/sipp -i $local_ip -sf /var/log/clearwater-sipp/sip-stress-registry-latency.xml bono:5060 -t tn -s default.svc.cluster.local -inf /usr/share/clearwater/sip-stress/user_test_latency.csv -users 1 -m 1 -default_behaviors all,-bye -max_socket 65000 -trace_stat -trace_rtt -trace_counts -trace_err -max_reconnect -1 -reconnect_sleep 0 -reconnect_close 0 -send_timeout 4000 -recv_timeout 12000 -nostdin"
+kubectl exec $sipptest /usr/share/clearwater/sip-stress/Agent_Latency.sh registry
 END=$(date +%s.%N)
 REGISTRY=$(echo "$END - $START" | bc)
 START=$(date +%s.%N)
-kubectl exec $sipptest "/usr/share/clearwater/bin/sipp -i $local_ip -sf /var/log/clearwater-sipp/sip-stress-call-latency.xml bono:5060 -t tn -s default.svc.cluster.local -inf /usr/share/clearwater/sip-stress/user_test_latency.csv -users 1 -m 1 -default_behaviors all,-bye -max_socket 65000 -trace_stat -trace_rtt -trace_counts -trace_err -max_reconnect -1 -reconnect_sleep 0 -reconnect_close 0 -send_timeout 4000 -recv_timeout 12000 -nostdin"
+kubectl exec $sipptest /usr/share/clearwater/sip-stress/Agent_Latency.sh call
 END=$(date +%s.%N)
 CALL_LENGHT=$(echo "$END - $START" | bc)
 START=$(date +%s.%N)
-kubectl exec $sipptest "/usr/share/clearwater/bin/sipp -i $local_ip -sf /var/log/clearwater-sipp/sip-stress-end-latency.xml bono:5060 -t tn -s default.svc.cluster.local -inf /usr/share/clearwater/sip-stress/user_test_latency.csv -users 1 -m 1 -default_behaviors all,-bye -max_socket 65000 -trace_stat -trace_rtt -trace_counts -trace_err -max_reconnect -1 -reconnect_sleep 0 -reconnect_close 0 -send_timeout 4000 -recv_timeout 12000 -nostdin"
+kubectl exec $sipptest /usr/share/clearwater/sip-stress/Agent_Latency.sh bye
 END=$(date +%s.%N)
 BYE_LENGHT=$(echo "$END - $START" | bc)
 
