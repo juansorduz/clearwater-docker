0cb1457650e5063adc1426177196b91d87855ab1
juansorduz <juanorduz@unicauca.edu.co>
2019-04-16 12:12:58 -0500

    Adding monitor scripts DC

--- ScriptsDat/Latency/Tester_Latency.sh
+++ ScriptsDat/Latency/Tester_Latency.sh
@@ -6,7 +6,7 @@ sipptest=$(kubectl get pods | grep sipptest | cut -d ' ' -f1)
 #kubectl cp $directory/sip-stress-call-latency.xml $sipptest:/usr/share/clearwater/sip-stress/
 #kubectl cp $directory/sip-stress-bye-latency.xml $sipptest:/usr/share/clearwater/sip-stress/
 cp $directory/sip-stress-latency-template $directory/sip-stress-latency
-sed -i "70s@192.168.190.20@$ip@" $directory/sip-stress-latency
+sed -i "69s@192.168.190.20@$ip@" $directory/sip-stress-latency
 #kubectl cp $directory/sip-stress-complete-latency.xml $sipptest:/usr/share/clearwater/sip-stress/
 echo Sending csv file for single test $directory/users_test_latency.csv.1
 kubectl cp $directory/users_test_latency.csv.1 $sipptest:/usr/share/clearwater/sip-stress/
