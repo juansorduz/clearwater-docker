01a17644ccbe3af7a847268a5caab4239ebb5d3e
juansorduz <juanorduz@unicauca.edu.co>
2019-04-16 12:03:13 -0500

    Adding monitor scripts DC

--- ScriptsDat/Latency/Tester_Latency.sh
+++ ScriptsDat/Latency/Tester_Latency.sh
@@ -11,12 +11,12 @@ sed -i "70s@192.168.190.20@$ip@" $directory/sip-stress-latency
 echo Sending csv file for single test $directory/users_test_latency.csv.1
 kubectl cp $directory/users_test_latency.csv.1 $sipptest:/usr/share/clearwater/sip-stress/
 echo Sending single latency script
-kubectl cp $directory/sip-stress-latency $sipptest:/usr/share/clearwater/sip-stress/bin/
+kubectl cp $directory/sip-stress-latency $sipptest:/usr/share/clearwater/bin
 
 kubectl exec $sipptest chmod 777 /usr/share/clearwater/sip-stress/users_test_latency.csv.1
-kubectl exec $sipptest chmod 777 /usr/share/clearwater/sip-stress/bin/sip-stress-latency
+kubectl exec $sipptest chmod 777 /usr/share/clearwater/bin/sip-stress-latency
 kubectl exec $sipptest chmod +x /usr/share/clearwater/sip-stress/users_test_latency.csv.1
-kubectl exec $sipptest chmod +x /usr/share/clearwater/sip-stress/bin/sip-stress-latency
+kubectl exec $sipptest chmod +x /usr/share/clearwater/bin/sip-stress-latency
 
 [ -e $testfolder/SingleLatencyTest.csv ] && rm $testfolder/SingleLatencyTest.csv
 touch $testfolder/SingleLatencyTest.csv
@@ -24,7 +24,7 @@ source $testfolder/Variables.txt
 while [ "$stateTest" -eq '1' ]; do
 #echo REGISTRY
   START=$(date +%s.%N)
-  kubectl exec $sipptest ./usr/share/clearwater/sip-stress/bin/sip-stress-latency > /dev/null
+  kubectl exec $sipptest ./usr/share/clearwater/bin/sip-stress-latency > /dev/null
   END=$(date +%s.%N)
   LATENCY=$(echo "$END - $START" | bc)
   echo $LATENCY >> $testfolder/SingleLatencyTest.csv
