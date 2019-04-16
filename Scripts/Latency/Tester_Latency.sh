cf5a41481d17daecb482f294d1fc146a89139be7
juansorduz <juanorduz@unicauca.edu.co>
2019-04-15 19:01:30 -0500

    Updating Latency Script

--- Scripts/Latency/Tester_Latency.sh
+++ Scripts/Latency/Tester_Latency.sh
@@ -1,26 +1,18 @@
 #!/bin/bash
-
+ip=${3:-127.0.0.1}
 directory=~/clearwater-docker/Scripts/Latency
 sipptest=$(kubectl get pods | grep sipptest | cut -d ' ' -f1)
-kubectl cp $directory/sip-stress-registry-latency.xml $sipptest:/usr/share/clearwater/sip-stress/
-kubectl cp $directory/sip-stress-call-latency.xml $sipptest:/usr/share/clearwater/sip-stress/
-kubectl cp $directory/sip-stress-bye-latency.xml $sipptest:/usr/share/clearwater/sip-stress/
+#kubectl cp $directory/sip-stress-registry-latency.xml $sipptest:/usr/share/clearwater/sip-stress/
+#kubectl cp $directory/sip-stress-call-latency.xml $sipptest:/usr/share/clearwater/sip-stress/
+#kubectl cp $directory/sip-stress-bye-latency.xml $sipptest:/usr/share/clearwater/sip-stress/
+cp $directory/sip-stress-latency-template $directory/sip-stress-latency
+sed -i "70s@192.168.190.20@$ip@" $directory/sip-stress-latency
+kubectl cp $directory/sip-stress-complete-latency.xml $sipptest:/usr/share/clearwater/sip-stress/
 kubectl cp $directory/users_test_latency.csv $sipptest:/usr/share/clearwater/sip-stress/
-kubectl cp $directory/Agent_Latency.sh $sipptest:/usr/share/clearwater/sip-stress/
+kubectl cp $directory/sip-stress-latency $sipptest:/usr/share/clearwater/sip-stress/
+
 echo REGISTRY
 START=$(date +%s.%N)
-kubectl exec -ti $sipptest ./usr/share/clearwater/sip-stress/Agent_Latency.sh registry
+kubectl exec -ti $sipptest ./usr/share/clearwater/sip-stress/sip-stress-latency
 END=$(date +%s.%N)
-REGISTRY=$(echo "$END - $START" | bc)
-echo Call
-START=$(date +%s.%N)
-kubectl exec -ti $sipptest ./usr/share/clearwater/sip-stress/Agent_Latency.sh call
-END=$(date +%s.%N)
-CALL_LENGHT=$(echo "$END - $START" | bc)
-echo bye
-START=$(date +%s.%N)
-kubectl exec -ti $sipptest ./usr/share/clearwater/sip-stress/Agent_Latency.sh bye
-END=$(date +%s.%N)
-BYE_LENGHT=$(echo "$END - $START" | bc)
-
-echo Registro: $REGISTRY CALL_LENGHT: $CALL_LENGHT BYE_LENGHT: $BYE_LENGHT
+LATENCY=$(echo "$END - $START" | bc)
