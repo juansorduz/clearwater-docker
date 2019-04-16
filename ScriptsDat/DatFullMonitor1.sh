a6a9b7dbb2aae44d93be69c90a1136d797b82662
juansorduz <juanorduz@unicauca.edu.co>
2019-04-16 11:22:15 -0500

    Adding monitor scripts DC

--- ScriptsDat/DatFullMonitor1.sh
+++ ScriptsDat/DatFullMonitor1.sh
@@ -5,7 +5,16 @@ password=${2:-secret}
 option=${3:-3}
 NumberTest=${4:-2}
 
-rm -r ~/ClearwaterTestResults
+rm -r ~/ClearwaterTestResults/Kubernetes4
+
+source ~/clearwater-docker/ScriptsDat/LocalFiles/AddressVM
+sshpass -p $password ssh -t -o StrictHostKeyChecking=no worker1@$AddressVM1 "rm -r ~/ClearwaterTestResults/Kubernetes4"
+sshpass -p $password ssh -t -o StrictHostKeyChecking=no worker2@$AddressVM2 "rm -r ~/ClearwaterTestResults/Kubernetes4"
+sshpass -p $password ssh -t -o StrictHostKeyChecking=no worker3@$AddressVM3 "rm -r ~/ClearwaterTestResults/Kubernetes4"
+sshpass -p $password ssh -t -o StrictHostKeyChecking=no worker4@$AddressVM4 "rm -r ~/ClearwaterTestResults/Kubernetes4"
+sshpass -p $password ssh -t -o StrictHostKeyChecking=no worker5@$AddressVM5 "rm -r ~/ClearwaterTestResults/Kubernetes4"
+sshpass -p $password ssh -t -o StrictHostKeyChecking=no worker6@$AddressVM6 "rm -r ~/ClearwaterTestResults/Kubernetes4"
+sshpass -p $password ssh -t -o StrictHostKeyChecking=no worker7@$AddressVM7 "rm -r ~/ClearwaterTestResults/Kubernetes4"
 
 . ~/clearwater-docker/ScriptsDat/DatMonitor1.sh  10 150 $ip $password $option $NumberTest
 #sleep 30
