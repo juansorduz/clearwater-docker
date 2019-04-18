4b00803283f200c74ecde4ba8b87a071799c2fc3
juansorduz <juanorduz@unicauca.edu.co>
2019-04-18 07:05:56 -0500

    Adding monitor scripts DC

--- ScriptsDat/DatFullPromedy1.sh
+++ ScriptsDat/DatFullPromedy1.sh
@@ -1,67 +1,67 @@
 #!/bin/bash
 #CONTADOR=0
-Duration=${1:-120}
+duration=${1:-120}
 NumberTest=${2:-2}
 
 #rm -r ~/ClearwaterTestResults
-echo Duration $Duration NumberTest $NumberTest
+echo duration $duration NumberTest $NumberTest
 
-. ~/clearwater-docker/ScriptsDat/DatGeneralDataPromedy1.sh  10 $Duration $NumberTest
-. ~/clearwater-docker/ScriptsDat/DatDockerDataPromedy1.sh  10 $Duration $NumberTest
-. ~/clearwater-docker/ScriptsDat/DatVMDataPromedy1.sh  10 $Duration $NumberTest
+. ~/clearwater-docker/ScriptsDat/DatGeneralDataPromedy1.sh  10 $duration $NumberTest
+. ~/clearwater-docker/ScriptsDat/DatDockerDataPromedy1.sh  10 $duration $NumberTest
+. ~/clearwater-docker/ScriptsDat/DatVMDataPromedy1.sh  10 $duration $NumberTest
 
-. ~/clearwater-docker/ScriptsDat/DatGeneralDataPromedy1.sh  20 $Duration $NumberTest
-. ~/clearwater-docker/ScriptsDat/DatDockerDataPromedy1.sh  20 $Duration $NumberTest
-. ~/clearwater-docker/ScriptsDat/DatVMDataPromedy1.sh  20 $Duration $NumberTest
+. ~/clearwater-docker/ScriptsDat/DatGeneralDataPromedy1.sh  20 $duration $NumberTest
+. ~/clearwater-docker/ScriptsDat/DatDockerDataPromedy1.sh  20 $duration $NumberTest
+. ~/clearwater-docker/ScriptsDat/DatVMDataPromedy1.sh  20 $duration $NumberTest
 
-. ~/clearwater-docker/ScriptsDat/DatGeneralDataPromedy1.sh  30 $Duration $NumberTest
-. ~/clearwater-docker/ScriptsDat/DatDockerDataPromedy1.sh  30 $Duration $NumberTest
-. ~/clearwater-docker/ScriptsDat/DatVMDataPromedy1.sh  30 $Duration $NumberTest
+. ~/clearwater-docker/ScriptsDat/DatGeneralDataPromedy1.sh  30 $duration $NumberTest
+. ~/clearwater-docker/ScriptsDat/DatDockerDataPromedy1.sh  30 $duration $NumberTest
+. ~/clearwater-docker/ScriptsDat/DatVMDataPromedy1.sh  30 $duration $NumberTest
 
-. ~/clearwater-docker/ScriptsDat/DatGeneralDataPromedy1.sh  40 $Duration $NumberTest
-. ~/clearwater-docker/ScriptsDat/DatDockerDataPromedy1.sh  40 $Duration $NumberTest
-. ~/clearwater-docker/ScriptsDat/DatVMDataPromedy1.sh  40 $Duration $NumberTest
+. ~/clearwater-docker/ScriptsDat/DatGeneralDataPromedy1.sh  40 $duration $NumberTest
+. ~/clearwater-docker/ScriptsDat/DatDockerDataPromedy1.sh  40 $duration $NumberTest
+. ~/clearwater-docker/ScriptsDat/DatVMDataPromedy1.sh  40 $duration $NumberTest
 
-. ~/clearwater-docker/ScriptsDat/DatGeneralDataPromedy1.sh  50 $Duration $NumberTest
-. ~/clearwater-docker/ScriptsDat/DatDockerDataPromedy1.sh  50 $Duration $NumberTest
-. ~/clearwater-docker/ScriptsDat/DatVMDataPromedy1.sh  50 $Duration $NumberTest
+. ~/clearwater-docker/ScriptsDat/DatGeneralDataPromedy1.sh  50 $duration $NumberTest
+. ~/clearwater-docker/ScriptsDat/DatDockerDataPromedy1.sh  50 $duration $NumberTest
+. ~/clearwater-docker/ScriptsDat/DatVMDataPromedy1.sh  50 $duration $NumberTest
 
-. ~/clearwater-docker/ScriptsDat/DatGeneralDataPromedy1.sh  60 $Duration $NumberTest
-. ~/clearwater-docker/ScriptsDat/DatDockerDataPromedy1.sh  60 $Duration $NumberTest
-. ~/clearwater-docker/ScriptsDat/DatVMDataPromedy1.sh  60 $Duration $NumberTest
+. ~/clearwater-docker/ScriptsDat/DatGeneralDataPromedy1.sh  60 $duration $NumberTest
+. ~/clearwater-docker/ScriptsDat/DatDockerDataPromedy1.sh  60 $duration $NumberTest
+. ~/clearwater-docker/ScriptsDat/DatVMDataPromedy1.sh  60 $duration $NumberTest
 
-. ~/clearwater-docker/ScriptsDat/DatGeneralDataPromedy1.sh  70 $Duration $NumberTest
-. ~/clearwater-docker/ScriptsDat/DatDockerDataPromedy1.sh  70 $Duration $NumberTest
-. ~/clearwater-docker/ScriptsDat/DatVMDataPromedy1.sh  70 $Duration $NumberTest
+. ~/clearwater-docker/ScriptsDat/DatGeneralDataPromedy1.sh  70 $duration $NumberTest
+. ~/clearwater-docker/ScriptsDat/DatDockerDataPromedy1.sh  70 $duration $NumberTest
+. ~/clearwater-docker/ScriptsDat/DatVMDataPromedy1.sh  70 $duration $NumberTest
 
-. ~/clearwater-docker/ScriptsDat/DatGeneralDataPromedy1.sh  80 $Duration $NumberTest
-. ~/clearwater-docker/ScriptsDat/DatDockerDataPromedy1.sh  80 $Duration $NumberTest
-. ~/clearwater-docker/ScriptsDat/DatVMDataPromedy1.sh  80 $Duration $NumberTest
+. ~/clearwater-docker/ScriptsDat/DatGeneralDataPromedy1.sh  80 $duration $NumberTest
+. ~/clearwater-docker/ScriptsDat/DatDockerDataPromedy1.sh  80 $duration $NumberTest
+. ~/clearwater-docker/ScriptsDat/DatVMDataPromedy1.sh  80 $duration $NumberTest
 
-. ~/clearwater-docker/ScriptsDat/DatGeneralDataPromedy1.sh  90 $Duration $NumberTest
-. ~/clearwater-docker/ScriptsDat/DatDockerDataPromedy1.sh  90 $Duration $NumberTest
-. ~/clearwater-docker/ScriptsDat/DatVMDataPromedy1.sh  90 $Duration $NumberTest
+. ~/clearwater-docker/ScriptsDat/DatGeneralDataPromedy1.sh  90 $duration $NumberTest
+. ~/clearwater-docker/ScriptsDat/DatDockerDataPromedy1.sh  90 $duration $NumberTest
+. ~/clearwater-docker/ScriptsDat/DatVMDataPromedy1.sh  90 $duration $NumberTest
 
-. ~/clearwater-docker/ScriptsDat/DatGeneralDataPromedy1.sh  100 $Duration $NumberTest
-. ~/clearwater-docker/ScriptsDat/DatDockerDataPromedy1.sh  100 $Duration $NumberTest
-. ~/clearwater-docker/ScriptsDat/DatVMDataPromedy1.sh  100 $Duration $NumberTest
+. ~/clearwater-docker/ScriptsDat/DatGeneralDataPromedy1.sh  100 $duration $NumberTest
+. ~/clearwater-docker/ScriptsDat/DatDockerDataPromedy1.sh  100 $duration $NumberTest
+. ~/clearwater-docker/ScriptsDat/DatVMDataPromedy1.sh  100 $duration $NumberTest
 
-. ~/clearwater-docker/ScriptsDat/DatGeneralDataPromedy1.sh  120 $Duration $NumberTest
-. ~/clearwater-docker/ScriptsDat/DatDockerDataPromedy1.sh  120 $Duration $NumberTest
-. ~/clearwater-docker/ScriptsDat/DatVMDataPromedy1.sh  120 $Duration $NumberTest
+. ~/clearwater-docker/ScriptsDat/DatGeneralDataPromedy1.sh  120 $duration $NumberTest
+. ~/clearwater-docker/ScriptsDat/DatDockerDataPromedy1.sh  120 $duration $NumberTest
+. ~/clearwater-docker/ScriptsDat/DatVMDataPromedy1.sh  120 $duration $NumberTest
 
-. ~/clearwater-docker/ScriptsDat/DatGeneralDataPromedy1.sh  150 $Duration $NumberTest
-. ~/clearwater-docker/ScriptsDat/DatDockerDataPromedy1.sh  150 $Duration $NumberTest
-. ~/clearwater-docker/ScriptsDat/DatVMDataPromedy1.sh  150 $Duration $NumberTest
+. ~/clearwater-docker/ScriptsDat/DatGeneralDataPromedy1.sh  150 $duration $NumberTest
+. ~/clearwater-docker/ScriptsDat/DatDockerDataPromedy1.sh  150 $duration $NumberTest
+. ~/clearwater-docker/ScriptsDat/DatVMDataPromedy1.sh  150 $duration $NumberTest
 
-. ~/clearwater-docker/ScriptsDat/DatGeneralDataPromedy1.sh  200 $Duration $NumberTest
-. ~/clearwater-docker/ScriptsDat/DatDockerDataPromedy1.sh  200 $Duration $NumberTest
-. ~/clearwater-docker/ScriptsDat/DatVMDataPromedy1.sh  200 $Duration $NumberTest
+. ~/clearwater-docker/ScriptsDat/DatGeneralDataPromedy1.sh  200 $duration $NumberTest
+. ~/clearwater-docker/ScriptsDat/DatDockerDataPromedy1.sh  200 $duration $NumberTest
+. ~/clearwater-docker/ScriptsDat/DatVMDataPromedy1.sh  200 $duration $NumberTest
 
-. ~/clearwater-docker/ScriptsDat/DatGeneralDataPromedy1.sh  250 $Duration $NumberTest
-. ~/clearwater-docker/ScriptsDat/DatDockerDataPromedy1.sh  250 $Duration $NumberTest
-. ~/clearwater-docker/ScriptsDat/DatVMDataPromedy1.sh  250 $Duration $NumberTest
+. ~/clearwater-docker/ScriptsDat/DatGeneralDataPromedy1.sh  250 $duration $NumberTest
+. ~/clearwater-docker/ScriptsDat/DatDockerDataPromedy1.sh  250 $duration $NumberTest
+. ~/clearwater-docker/ScriptsDat/DatVMDataPromedy1.sh  250 $duration $NumberTest
 
-. ~/clearwater-docker/ScriptsDat/DatGeneralDataPromedy1.sh  300 $Duration $NumberTest
-. ~/clearwater-docker/ScriptsDat/DatDockerDataPromedy1.sh  300 $Duration $NumberTest
-. ~/clearwater-docker/ScriptsDat/DatVMDataPromedy1.sh  300 $Duration $NumberTest
+. ~/clearwater-docker/ScriptsDat/DatGeneralDataPromedy1.sh  300 $duration $NumberTest
+. ~/clearwater-docker/ScriptsDat/DatDockerDataPromedy1.sh  300 $duration $NumberTest
+. ~/clearwater-docker/ScriptsDat/DatVMDataPromedy1.sh  300 $duration $NumberTest
