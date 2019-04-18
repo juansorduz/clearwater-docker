8b76e16452383dff9d0f400ec760b904b2e9b74c
juansorduz <juanorduz@unicauca.edu.co>
2019-04-18 10:46:04 -0500

    Updating Latency Script

--- ScriptsKU/KUStart1.sh
+++ ScriptsKU/KUStart1.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 password=${1:-0}
-echo $password | sudo -S kubeadm reset -f
-rm -rf ~/.kube/
-echo $password | sudo -S kubeadm init --pod-network-cidr=192.168.0.0/16
-mkdir -p $HOME/.kube
-echo $password | sudo -S cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-echo $password | sudo -S chown $(id -u):$(id -g) $HOME/.kube/config
-kubectl taint nodes --all node-role.kubernetes.io/master-
-kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
-kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml
-sleep 5
-cd
+# echo $password | sudo -S kubeadm reset -f
+# rm -rf ~/.kube/
+# echo $password | sudo -S kubeadm init --pod-network-cidr=192.168.0.0/16
+# mkdir -p $HOME/.kube
+# echo $password | sudo -S cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+# echo $password | sudo -S chown $(id -u):$(id -g) $HOME/.kube/config
+# kubectl taint nodes --all node-role.kubernetes.io/master-
+# kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
+# kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml
+# sleep 5
+# cd
 kubectl create configmap env-vars --from-literal=ZONE=default.svc.cluster.local
 kubectl apply -f clearwater-docker/kubernetes
 sleep 300
