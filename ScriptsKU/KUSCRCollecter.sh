f8a7543026f4398f77cf9cbe4f1380cd5b207d3f
juansorduz <GabrielOrozco>
2019-04-22 13:34:37 -0500

    Script automated tester SCR

---
+++ ScriptsKU/KUSCRCollecter.sh
@@ -0,0 +1,26 @@
+testfolder=~/ClearwaterTestResults/Kubernetes3/
+NumTest=1
+
+for i in `seq 15`; do
+rm -r ~/ClearwaterTestResults/Kubernetes3/*
+./ScriptsKU/KUMonitor_independent.sh 120 30 10.244.4.14 2 sipptest-8548ccd744-pzw72 &
+./ScriptsKU/KUMonitor_independent.sh 60 30 10.244.3.14 2 sipptest-8548ccd744-8ctdr &
+./ScriptsKU/KUMonitor_independent.sh 50 30 10.244.1.17 2 sipptest-8548ccd744-mmpqs &
+./ScriptsKU/KUMonitor_independent.sh 30 30 10.244.2.15 2 sipptest-8548ccd744-hnxhf &
+./ScriptsKU/KUMonitor_independent.sh 40 30 10.244.6.7 2 sipptest-8548ccd744-r5vrt &
+NumTest++
+for j in 12030 6030 5030 3030 4030 ; do
+CallGenerate=$(grep -F "OutGoing call created" $testfolder/$j/1/logsSIPpTest.txt | cut -d '|' -f3)
+SuccesfullCall=$(grep -F "Successful call" $testfolder/$j/1/logsSIPpTest.txt | cut -d '|' -f3)
+FailedCall=$(grep -F "Failed call" $testfolder/$j/1/logsSIPpTest.txt | cut -d '|' -f3)
+Scale=100
+CallGenerate=${CallGenerate::-1}
+SuccesfullCall=${SuccesfullCall::-1}
+FailedCall=${FailedCall::-1}
+SuccesfullCallRate=$(echo "scale=3; $SuccesfullCall*$Scale" | bc -l)
+SuccesfullCallRate=$(echo "scale=2; $SuccesfullCallRate/$CallGenerate" | bc -l)
+echo $SuccesfullCallRate >> $testfolder/PromediosSCPS$cps
+echo Call Generate:$CallGenerate SuccesfullCall:$SuccesfullCall FailedCall $FailedCall SCR:$SuccesfullCallRate
+sleep 45
+done
+done
