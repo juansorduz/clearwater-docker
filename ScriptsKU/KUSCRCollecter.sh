dab836c1787f98db470692ad9b19e88a2bdfabf6
juansorduz <GabrielOrozco>
2019-04-22 15:24:06 -0500

    Script automated tester SCR

--- ScriptsKU/KUSCRCollecter.sh
+++ ScriptsKU/KUSCRCollecter.sh
@@ -3,13 +3,14 @@ NumTest=1
 
 for i in `seq 15`; do
 rm -r ~/ClearwaterTestResults/Kubernetes3/*
-./ScriptsKU/KUMonitor_independent.sh 120 30 10.244.4.14 2 sipptest-8548ccd744-pzw72 &
-./ScriptsKU/KUMonitor_independent.sh 60 30 10.244.3.14 2 sipptest-8548ccd744-8ctdr &
-./ScriptsKU/KUMonitor_independent.sh 50 30 10.244.1.17 2 sipptest-8548ccd744-mmpqs &
-./ScriptsKU/KUMonitor_independent.sh 30 30 10.244.2.15 2 sipptest-8548ccd744-hnxhf &
-./ScriptsKU/KUMonitor_independent.sh 40 30 10.244.6.7 2 sipptest-8548ccd744-r5vrt &
+./ScriptsKU/KUMonitor_independent.sh 110 30 10.244.4.14 2 sipptest-8548ccd744-pzw72 &
+./ScriptsKU/KUMonitor_independent.sh 50 30 10.244.3.14 2 sipptest-8548ccd744-8ctdr &
+./ScriptsKU/KUMonitor_independent.sh 40 30 10.244.1.17 2 sipptest-8548ccd744-mmpqs &
+./ScriptsKU/KUMonitor_independent.sh 20 30 10.244.2.15 2 sipptest-8548ccd744-hnxhf &
+./ScriptsKU/KUMonitor_independent.sh 30 30 10.244.6.7 2 sipptest-8548ccd744-r5vrt &
 sum=0
 sleep 5
+mkdir ~/ClearwaterTestResults/250
 while [ "$sum" -lt '10' ]; do
   echo hola
   sum=0
@@ -35,8 +36,8 @@ SuccesfullCall=${SuccesfullCall::-1}
 FailedCall=${FailedCall::-1}
 SuccesfullCallRate=$(echo "scale=3; $SuccesfullCall*$Scale" | bc -l)
 SuccesfullCallRate=$(echo "scale=2; $SuccesfullCallRate/$CallGenerate" | bc -l)
-mkdir ~/ClearwaterTestResults/$j
-echo $SuccesfullCallRate >> ~/ClearwaterTestResults/$j/PromediosSCPS$i
+mkdir ~/ClearwaterTestResults/250/$j
+echo $SuccesfullCallRate >> ~/ClearwaterTestResults/250/$j/PromediosSCPS$i
 echo Call Generate:$CallGenerate SuccesfullCall:$SuccesfullCall FailedCall $FailedCall SCR:$SuccesfullCallRate
 done
 done
