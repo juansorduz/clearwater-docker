72630b94aa84b4d22a338809635101bc1433af53
juansorduz <juanorduz@unicauca.edu.co>
2019-04-22 23:11:06 -0500

    Updating SCR script

--- ScriptsKU/KUSCRCollecter.sh
+++ ScriptsKU/KUSCRCollecter.sh
@@ -3,30 +3,30 @@ NumTest=1
 
 for i in `seq 15`; do
 rm -r ~/ClearwaterTestResults/Kubernetes3/*
-./ScriptsKU/KUMonitor_independent.sh 45 30 10.244.4.14 2 sipptest-8548ccd744-pzw72 &
-./ScriptsKU/KUMonitor_independent.sh 50 30 10.244.3.14 2 sipptest-8548ccd744-8ctdr &
-./ScriptsKU/KUMonitor_independent.sh 35 30 10.244.1.17 2 sipptest-8548ccd744-mmpqs &
-./ScriptsKU/KUMonitor_independent.sh 40 30 10.244.2.15 2 sipptest-8548ccd744-hnxhf &
-./ScriptsKU/KUMonitor_independent.sh 30 30 10.244.6.7 2 sipptest-8548ccd744-r5vrt &
+./ScriptsKU/KUMonitor_independent.sh 35 30 10.244.4.14 2 sipptest-8548ccd744-pzw72 &
+./ScriptsKU/KUMonitor_independent.sh 40 30 10.244.3.14 2 sipptest-8548ccd744-8ctdr &
+./ScriptsKU/KUMonitor_independent.sh 25 30 10.244.1.17 2 sipptest-8548ccd744-mmpqs &
+./ScriptsKU/KUMonitor_independent.sh 30 30 10.244.2.15 2 sipptest-8548ccd744-hnxhf &
+./ScriptsKU/KUMonitor_independent.sh 20 30 10.244.6.7 2 sipptest-8548ccd744-r5vrt &
 sum=0
 sleep 5
-mkdir ~/ClearwaterTestResults/200
+mkdir ~/ClearwaterTestResults/1500
 while [ "$sum" -lt '10' ]; do
   echo hola
   sum=0
-  source $testfolder/4530/1/Variables.txt
-  sum=`echo $sum + $stateTest | bc`
-  source $testfolder/7030/1/Variables.txt
-  sum=`echo $sum + $stateTest | bc`
   source $testfolder/3530/1/Variables.txt
   sum=`echo $sum + $stateTest | bc`
-  source $testfolder/4030/1/Variables.txt
+  source $testfolder/6030/1/Variables.txt
+  sum=`echo $sum + $stateTest | bc`
+  source $testfolder/2530/1/Variables.txt
   sum=`echo $sum + $stateTest | bc`
   source $testfolder/3030/1/Variables.txt
   sum=`echo $sum + $stateTest | bc`
+  source $testfolder/2030/1/Variables.txt
+  sum=`echo $sum + $stateTest | bc`
   echo sum=$sum
 done
-for j in 4530 5030 3530 4030 3030 ; do
+for j in 3530 4030 2530 3030 2030 ; do
 CallGenerate=$(grep -F "OutGoing call created" $testfolder/$j/1/logsSIPpTest.txt | cut -d '|' -f3)
 SuccesfullCall=$(grep -F "Successful call" $testfolder/$j/1/logsSIPpTest.txt | cut -d '|' -f3)
 FailedCall=$(grep -F "Failed call" $testfolder/$j/1/logsSIPpTest.txt | cut -d '|' -f3)
@@ -36,8 +36,8 @@ SuccesfullCall=${SuccesfullCall::-1}
 FailedCall=${FailedCall::-1}
 SuccesfullCallRate=$(echo "scale=3; $SuccesfullCall*$Scale" | bc -l)
 SuccesfullCallRate=$(echo "scale=2; $SuccesfullCallRate/$CallGenerate" | bc -l)
-mkdir ~/ClearwaterTestResults/200/$j
-echo $SuccesfullCallRate >> ~/ClearwaterTestResults/200/$j/PromediosSCPS$i
+mkdir ~/ClearwaterTestResults/150/$j
+echo $SuccesfullCallRate >> ~/ClearwaterTestResults/150/$j/PromediosSCPS$i
 echo Call Generate:$CallGenerate SuccesfullCall:$SuccesfullCall FailedCall $FailedCall SCR:$SuccesfullCallRate
 done
 done
