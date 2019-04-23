b8b3713be7a8d7f787c2c65a9c67fa7a40ac0259
juansorduz <GabrielOrozco>
2019-04-23 14:46:40 -0500

    Summarizer SCR

--- ScriptsDat/SCR/summarizer_SCR.sh
+++ ScriptsDat/SCR/summarizer_SCR.sh
@@ -1,4 +1,4 @@
-testfolder=~/ClearwaterTestResults/
+testfolder=~/ClearwaterTestResults
 for k  in 5bono_original 5bono_original_2sprout 5bono_original_3sprout 5bono 5bono2ur_mscsprout 5bono3ur_mscsprout; do
 for j in 100 150 200 250 300 ; do
 sum=0
