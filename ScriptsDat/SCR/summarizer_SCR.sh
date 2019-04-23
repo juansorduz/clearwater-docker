a3522148aa85370dc0f1415524b1509990c3eeed
juansorduz <juanorduz@unicauca.edu.co>
2019-04-23 13:29:48 -0500

    Updating SCR script

--- ScriptsDat/SCR/summarizer_SCR.sh
+++ ScriptsDat/SCR/summarizer_SCR.sh
@@ -1,7 +1,8 @@
 testfolder=~/ClearwaterTestResults/
-for k 
-for j in 100 150 200 250 300 ; ; do
-for i in `seq 14`; ; do
-cat $testfolder/$k/$j/*/PromediosSCPS$i > $testfolder/$j/sumary_prueba$i.csv
+for k  in 5bono_original 5bono_original_2sprout 5bono_original_3sprout 5bono 5bono2ur_mscsprout 5bono3ur_mscsprout; do
+for j in 100 150 200 250 300 ; do
+for i in `seq 15`; do
+cat $testfolder/$k/$j/*/PromediosSCPS$i > $testfolder/$k/$j/sumary_prueba$i.csv
+done
 done
 done
