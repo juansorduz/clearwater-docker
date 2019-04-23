56f23fc5c664f6ae03c04989f1bece63674eb24f
juansorduz <GabrielOrozco>
2019-04-23 14:45:14 -0500

    Summarizer SCR

--- ScriptsDat/SCR/summarizer_SCR.sh
+++ ScriptsDat/SCR/summarizer_SCR.sh
@@ -7,7 +7,7 @@ for i in `seq 15`; do
 #cat $testfolder/$k/$j/*/PromediosSCPS$i > $testfolder/$k/$j/sumary_prueba$i.csv
 sum="paste -sd+ $testfolder/$k/$j/sumary_prueba$i.csv | bc"
 result=$(($sum * 0.2))
-echo $result > $testfolder/$k/$j/promedi$i.csv
+echo $result >> $testfolder/$k/$j/promedi.csv
 done
 done
 done
