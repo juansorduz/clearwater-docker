feccef4a728497ca3d47f942e6b96908d4562f2d
juansorduz <juanorduz@unicauca.edu.co>
2019-04-26 14:53:21 -0500

    Adding monitor scripts DC

--- ScriptsDat2/Graphs/DatGraphMainVMCPSvsRAM1.sh
+++ ScriptsDat2/Graphs/DatGraphMainVMCPSvsRAM1.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-mkdir -p ~/ClearwaterTestResults/Kubernetes4/Graphs
-cd ~/ClearwaterTestResults/Kubernetes4/Graphs
+source ~/clearwater-docker/ScriptsDat2/TrafficGenerator/TestCharacteristics
+testMainfolder=$Maintestfolder/b${NumBono}urs${NumURS}mscs${NumMSCS}urh${NumURH}msch${NumMSCH}/
+testfolder=$Maintestfolder/b${NumBono}urs${NumURS}mscs${NumMSCS}urh${NumURH}msch${NumMSCH}/$cps$duration
+Graphfolder=$Maintestfolder/Graphs
+mkdir -p $Graphfolder
+cd $Graphfolder
 gnuplot <<- EOF
 set terminal png size 800,500 enhanced font "Helvetica,20"
 red = "#FF0000"; green = "#00FF00"; blue = "#0000FF"; skyblue = "#87CEEB";
-set yrange [0:75]
+set yrange [0:500]
 set style data histogram
 set style histogram cluster errorbars gap 1
 set style fill solid
@@ -13,17 +17,16 @@ set xtics format ""
 set grid ytics
 set key outside below
 set terminal pdf
-set output "DespliegueKU4VMSCPSvsRAM.pdf"
+set output "DespliegueVMCPSvsRAMb${NumBono}urs${NumURS}mscs${NumMSCS}urh${NumURH}msch${NumMSCH}.pdf"
 set xlabel "CPS"
 set ylabel "RAM (%)"
-set title "CPS vs RAM Deployment Kubernetes 4"
-plot "~/ClearwaterTestResults/Kubernetes4/SUMMARYVMRAM" \
-                  using 2:3:xtic(1) title "VM1" fill pattern 1 lc 8, \
-                  '' using 4:5:xtic(1) title "VM2" fill pattern 2 lc 8, \
-                  '' using 6:7:xtic(1) title "VM3" fill pattern 3 lc 8, \
-                  '' using 8:9:xtic(1) title "VM4" fill pattern 4 lc 8, \
-                  '' using 10:11:xtic(1) title "VM5" fill pattern 5 lc 8, \
-                  '' using 12:13:xtic(1) title "VM6" fill pattern 6 lc 8, \
-                  '' using 14:15:xtic(1) title "VM7" fill pattern 7 lc 8, \
-                  '' using 16:17:xtic(1) title "VM8" fill pattern 8 lc 8
+set title "CPS vs RAM Deployment Kubernetes"
+plot "$testMainfolder/SUMMARYVMRAM" \
+            using 2:3:xtic(1) title "VM1" fill pattern 1 lc 8, \
+            '' using 4:5:xtic(1) title "VM2" fill pattern 2 lc 8, \
+            '' using 6:7:xtic(1) title "VM3" fill pattern 3 lc 8, \
+            '' using 8:9:xtic(1) title "VM4" fill pattern 4 lc 8, \
+            '' using 10:11:xtic(1) title "VM5" fill pattern 5 lc 8, \
+            '' using 12:13:xtic(1) title "VM6" fill pattern 6 lc 8, \
+            '' using 14:15:xtic(1) title "VM7" fill pattern 7 lc 8
 EOF
