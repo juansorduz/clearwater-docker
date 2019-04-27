d89c33a087267654d3125c90b15360443b7a3e76
juansorduz <juanorduz@unicauca.edu.co>
2019-04-27 10:44:17 -0500

    Adding monitor scripts DC

--- ScriptsDat2/DatGeneralDataPromedy1.sh
+++ ScriptsDat2/DatGeneralDataPromedy1.sh
@@ -33,10 +33,10 @@ NumProms=0
 #LATENCY
 #############################################################################
 echo Calculating Latency
-DatosLatency=$(<$testfolder/PromediosDelay$cps)
+DatosLatency=$(<$testfolder/PromediosSingleLatency$cps)
 AverageLatency=0
 VarLatency=0
-NumProms=$(wc -l < $testfolder/PromediosDelay$cps)
+NumProms=$(wc -l < $testfolder/PromediosSingleLatency$cps)
 for i in ${DatosLatency[@]}; do  AverageLatency=$(echo "$AverageLatency + $i" | bc -l) ; done
 AverageLatency=$(echo "scale=3;$AverageLatency/$NumProms" | bc -l)
 for i in ${DatosLatency[@]}; do DiferenceVar=$(echo "$i - $AverageLatency" | bc -l);DiferenceVar=$(echo "$DiferenceVar* $DiferenceVar" | bc -l); VarLatency=$(echo "$VarLatency + $DiferenceVar" | bc -l) ; done
