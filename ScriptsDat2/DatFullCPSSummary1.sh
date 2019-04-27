56f217581d996b3c219075bdd619a6c89515014a
juansorduz <juanorduz@unicauca.edu.co>
2019-04-27 09:17:43 -0500

    Adding monitor scripts DC

--- ScriptsDat2/DatFullCPSSummary1.sh
+++ ScriptsDat2/DatFullCPSSummary1.sh
@@ -12,41 +12,41 @@ source ~/clearwater-docker/ScriptsDat2/TrafficGenerator/TestCharacteristics
 . ~/clearwater-docker/ScriptsDat2/DatDockerDataSummary1.sh  10 $duration $NumberTest
 . ~/clearwater-docker/ScriptsDat2/DatVMDataSummary1.sh  10 $duration $NumberTest
 
-. ~/clearwater-docker/ScriptsDat2/DatGeneralDataSummary1.sh  20 $duration $NumberTest
-. ~/clearwater-docker/ScriptsDat2/DatDockerDataSummary1.sh  20 $duration $NumberTest
-. ~/clearwater-docker/ScriptsDat2/DatVMDataSummary1.sh  20 $duration $NumberTest
+# . ~/clearwater-docker/ScriptsDat2/DatGeneralDataSummary1.sh  20 $duration $NumberTest
+# . ~/clearwater-docker/ScriptsDat2/DatDockerDataSummary1.sh  20 $duration $NumberTest
+# . ~/clearwater-docker/ScriptsDat2/DatVMDataSummary1.sh  20 $duration $NumberTest
 #exit 0
 . ~/clearwater-docker/ScriptsDat2/DatGeneralDataSummary1.sh  30 $duration $NumberTest
 . ~/clearwater-docker/ScriptsDat2/DatDockerDataSummary1.sh  30 $duration $NumberTest
 . ~/clearwater-docker/ScriptsDat2/DatVMDataSummary1.sh  30 $duration $NumberTest
 
-. ~/clearwater-docker/ScriptsDat2/DatGeneralDataSummary1.sh  40 $duration $NumberTest
-. ~/clearwater-docker/ScriptsDat2/DatDockerDataSummary1.sh  40 $duration $NumberTest
-. ~/clearwater-docker/ScriptsDat2/DatVMDataSummary1.sh  40 $duration $NumberTest
+# . ~/clearwater-docker/ScriptsDat2/DatGeneralDataSummary1.sh  40 $duration $NumberTest
+# . ~/clearwater-docker/ScriptsDat2/DatDockerDataSummary1.sh  40 $duration $NumberTest
+# . ~/clearwater-docker/ScriptsDat2/DatVMDataSummary1.sh  40 $duration $NumberTest
 
-. ~/clearwater-docker/ScriptsDat2/DatGeneralDataSummary1.sh  50 $duration $NumberTest
-. ~/clearwater-docker/ScriptsDat2/DatDockerDataSummary1.sh  50 $duration $NumberTest
-. ~/clearwater-docker/ScriptsDat2/DatVMDataSummary1.sh  50 $duration $NumberTest
+# . ~/clearwater-docker/ScriptsDat2/DatGeneralDataSummary1.sh  50 $duration $NumberTest
+# . ~/clearwater-docker/ScriptsDat2/DatDockerDataSummary1.sh  50 $duration $NumberTest
+# . ~/clearwater-docker/ScriptsDat2/DatVMDataSummary1.sh  50 $duration $NumberTest
 
 . ~/clearwater-docker/ScriptsDat2/DatGeneralDataSummary1.sh  60 $duration $NumberTest
 . ~/clearwater-docker/ScriptsDat2/DatDockerDataSummary1.sh  60 $duration $NumberTest
 . ~/clearwater-docker/ScriptsDat2/DatVMDataSummary1.sh  60 $duration $NumberTest
 
-. ~/clearwater-docker/ScriptsDat2/DatGeneralDataSummary1.sh  70 $duration $NumberTest
-. ~/clearwater-docker/ScriptsDat2/DatDockerDataSummary1.sh  70 $duration $NumberTest
-. ~/clearwater-docker/ScriptsDat2/DatVMDataSummary1.sh  70 $duration $NumberTest
-
-. ~/clearwater-docker/ScriptsDat2/DatGeneralDataSummary1.sh  80 $duration $NumberTest
-. ~/clearwater-docker/ScriptsDat2/DatDockerDataSummary1.sh  80 $duration $NumberTest
-. ~/clearwater-docker/ScriptsDat2/DatVMDataSummary1.sh  80 $duration $NumberTest
+# . ~/clearwater-docker/ScriptsDat2/DatGeneralDataSummary1.sh  70 $duration $NumberTest
+# . ~/clearwater-docker/ScriptsDat2/DatDockerDataSummary1.sh  70 $duration $NumberTest
+# . ~/clearwater-docker/ScriptsDat2/DatVMDataSummary1.sh  70 $duration $NumberTest
+#
+# . ~/clearwater-docker/ScriptsDat2/DatGeneralDataSummary1.sh  80 $duration $NumberTest
+# . ~/clearwater-docker/ScriptsDat2/DatDockerDataSummary1.sh  80 $duration $NumberTest
+# . ~/clearwater-docker/ScriptsDat2/DatVMDataSummary1.sh  80 $duration $NumberTest
 
 . ~/clearwater-docker/ScriptsDat2/DatGeneralDataSummary1.sh  90 $duration $NumberTest
 . ~/clearwater-docker/ScriptsDat2/DatDockerDataSummary1.sh  90 $duration $NumberTest
 . ~/clearwater-docker/ScriptsDat2/DatVMDataSummary1.sh  90 $duration $NumberTest
 
-. ~/clearwater-docker/ScriptsDat2/DatGeneralDataSummary1.sh  100 $duration $NumberTest
-. ~/clearwater-docker/ScriptsDat2/DatDockerDataSummary1.sh  100 $duration $NumberTest
-. ~/clearwater-docker/ScriptsDat2/DatVMDataSummary1.sh  100 $duration $NumberTest
+# . ~/clearwater-docker/ScriptsDat2/DatGeneralDataSummary1.sh  100 $duration $NumberTest
+# . ~/clearwater-docker/ScriptsDat2/DatDockerDataSummary1.sh  100 $duration $NumberTest
+# . ~/clearwater-docker/ScriptsDat2/DatVMDataSummary1.sh  100 $duration $NumberTest
 
 . ~/clearwater-docker/ScriptsDat2/DatGeneralDataSummary1.sh  120 $duration $NumberTest
 . ~/clearwater-docker/ScriptsDat2/DatDockerDataSummary1.sh  120 $duration $NumberTest
@@ -56,13 +56,21 @@ source ~/clearwater-docker/ScriptsDat2/TrafficGenerator/TestCharacteristics
 . ~/clearwater-docker/ScriptsDat2/DatDockerDataSummary1.sh  150 $duration $NumberTest
 . ~/clearwater-docker/ScriptsDat2/DatVMDataSummary1.sh  150 $duration $NumberTest
 
-. ~/clearwater-docker/ScriptsDat2/DatGeneralDataSummary1.sh  200 $duration $NumberTest
-. ~/clearwater-docker/ScriptsDat2/DatDockerDataSummary1.sh  200 $duration $NumberTest
-. ~/clearwater-docker/ScriptsDat2/DatVMDataSummary1.sh  200 $duration $NumberTest
+. ~/clearwater-docker/ScriptsDat2/DatGeneralDataSummary1.sh  180 $duration $NumberTest
+. ~/clearwater-docker/ScriptsDat2/DatDockerDataSummary1.sh  180 $duration $NumberTest
+. ~/clearwater-docker/ScriptsDat2/DatVMDataSummary1.sh  180 $duration $NumberTest
 
-. ~/clearwater-docker/ScriptsDat2/DatGeneralDataSummary1.sh  250 $duration $NumberTest
-. ~/clearwater-docker/ScriptsDat2/DatDockerDataSummary1.sh  250 $duration $NumberTest
-. ~/clearwater-docker/ScriptsDat2/DatVMDataSummary1.sh  250 $duration $NumberTest
+# . ~/clearwater-docker/ScriptsDat2/DatGeneralDataSummary1.sh  200 $duration $NumberTest
+# . ~/clearwater-docker/ScriptsDat2/DatDockerDataSummary1.sh  200 $duration $NumberTest
+# . ~/clearwater-docker/ScriptsDat2/DatVMDataSummary1.sh  200 $duration $NumberTest
+
+. ~/clearwater-docker/ScriptsDat2/DatGeneralDataSummary1.sh  240 $duration $NumberTest
+. ~/clearwater-docker/ScriptsDat2/DatDockerDataSummary1.sh  240 $duration $NumberTest
+. ~/clearwater-docker/ScriptsDat2/DatVMDataSummary1.sh  240 $duration $NumberTest
+
+# . ~/clearwater-docker/ScriptsDat2/DatGeneralDataSummary1.sh  250 $duration $NumberTest
+# . ~/clearwater-docker/ScriptsDat2/DatDockerDataSummary1.sh  250 $duration $NumberTest
+# . ~/clearwater-docker/ScriptsDat2/DatVMDataSummary1.sh  250 $duration $NumberTest
 
 . ~/clearwater-docker/ScriptsDat2/DatGeneralDataSummary1.sh  300 $duration $NumberTest
 . ~/clearwater-docker/ScriptsDat2/DatDockerDataSummary1.sh  300 $duration $NumberTest
