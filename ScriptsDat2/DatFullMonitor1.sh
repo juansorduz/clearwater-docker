44f8c728a943c172d581a2fb257d21c513fa7722
juansorduz <juanorduz@unicauca.edu.co>
2019-04-28 10:53:24 -0500

    Adding monitor scripts DC

--- ScriptsDat2/DatFullMonitor1.sh
+++ ScriptsDat2/DatFullMonitor1.sh
@@ -78,7 +78,7 @@ echo PRUEBA DE DESPLIEGUE CON b1u1s1
 echo -e "NumSipp=1\nNumBono=1\nNumURS=1\nNumMSCS=1\nNumURH=1\nNumMSCH=1\nMaintestfolder=~/ClearwaterTestResults/Kubernetes5" > ~/clearwater-docker/ScriptsDat2/TrafficGenerator/TestCharacteristics
 . ~/clearwater-docker/ScriptsDat2/DatFullCPSMonitor1.sh $Duration $password $option $NumberTest
 
-exit 0
+#exit 0
 
 echo PRUEBA DE DESPLIEGUE CON b2u1s1
 echo -e "NumSipp=2\nNumBono=2\nNumURS=1\nNumMSCS=1\nNumURH=1\nNumMSCH=1\nMaintestfolder=~/ClearwaterTestResults/Kubernetes5" > ~/clearwater-docker/ScriptsDat2/TrafficGenerator/TestCharacteristics
