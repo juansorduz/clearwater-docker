988d6e0252d120f2bab998fe79443eb7d828094c
juansorduz <GabrielOrozco>
2019-04-29 11:31:47 -0500

    Updating test scripts

--- ScriptsDat2/Graphs/DatGraphAllDeploymentsCPSvsDelay1.sh
+++ ScriptsDat2/Graphs/DatGraphAllDeploymentsCPSvsDelay1.sh
@@ -10,7 +10,7 @@ cd $Graphfolder
 gnuplot <<- EOF
 set terminal png size 800,500 enhanced font "Helvetica,20"
 red = "#FF0000"; green = "#00FF00"; blue = "#0000FF"; skyblue = "#87CEEB";
-set yrange [0:500]
+set yrange [0:200]
 set boxwidth 0.9
 set style line 1 lt 1 lw 1 pt 1 linecolor rgb "0x767676"
 set xtics format ""
