6a736d266d9348698fd3ed29ba19a04feda90cb0
juansorduz <GabrielOrozco>
2019-04-29 10:34:23 -0500

    Updating test scripts

--- ScriptsDat2/Graphs/DatGraphAllDeploymentsCPSvsSCR1.sh
+++ ScriptsDat2/Graphs/DatGraphAllDeploymentsCPSvsSCR1.sh
@@ -20,11 +20,11 @@ set output "DespliegueCPSvsSCRAllKubernetesDeployments.png"
 set xlabel "CPS"
 set ylabel "SCR (%)"
 set title "CPS vs SCR"
-plot "$Maintestfolder/b1urs1mscs1urh1msch1/SUMMARYSCR" using 2:xtic(1) title 'b1ur1msc1' w l lc 0
-plot "$Maintestfolder/b1urs1mscs2urh1msch1/SUMMARYSCR" using 2:xtic(1) title 'b1ur1msc2' w l lc 1
-plot "$Maintestfolder/b1urs2mscs1urh1msch1/SUMMARYSCR" using 2:xtic(1) title 'b1ur2msc1' w l lc 2
-plot "$Maintestfolder/b2urs1mscs1urh1msch1/SUMMARYSCR" using 2:xtic(1) title 'b2ur1msc1' w l lc 3
-plot "$Maintestfolder/b2urs1mscs2urh1msch1/SUMMARYSCR" using 2:xtic(1) title 'b2ur1msc2' w l lc 4
-plot "$Maintestfolder/b2urs2mscs1urh1msch1/SUMMARYSCR" using 2:xtic(1) title 'b2ur2msc1' w l lc 5
+plot "$Maintestfolder/b1urs1mscs1urh1msch1/SUMMARYSCR" using 2:xtic(1) title 'b1ur1msc1' w l lc 0,
+plot "$Maintestfolder/b1urs1mscs2urh1msch1/SUMMARYSCR" using 2:xtic(1) title 'b1ur1msc2' w l lc 1,
+plot "$Maintestfolder/b1urs2mscs1urh1msch1/SUMMARYSCR" using 2:xtic(1) title 'b1ur2msc1' w l lc 2,
+plot "$Maintestfolder/b2urs1mscs1urh1msch1/SUMMARYSCR" using 2:xtic(1) title 'b2ur1msc1' w l lc 3,
+plot "$Maintestfolder/b2urs1mscs2urh1msch1/SUMMARYSCR" using 2:xtic(1) title 'b2ur1msc2' w l lc 4,
+plot "$Maintestfolder/b2urs2mscs1urh1msch1/SUMMARYSCR" using 2:xtic(1) title 'b2ur2msc1' w l lc 5,
 plot "$Maintestfolder/b3urs1mscs1urh1msch1/SUMMARYSCR" using 2:xtic(1) title 'b3ur1msc1' w l lc 6
 EOF
