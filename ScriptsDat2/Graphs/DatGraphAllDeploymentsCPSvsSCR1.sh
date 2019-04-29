3583aedb42c8baa3328cb88fb013a750c221a8fc
juansorduz <GabrielOrozco>
2019-04-29 10:27:41 -0500

    Updating test scripts && git push origin kubernetes10

--- ScriptsDat2/Graphs/DatGraphAllDeploymentsCPSvsSCR1.sh
+++ ScriptsDat2/Graphs/DatGraphAllDeploymentsCPSvsSCR1.sh
@@ -20,11 +20,11 @@ set output "DespliegueCPSvsSCRAllKubernetesDeployments.png"
 set xlabel "CPS"
 set ylabel "SCR (%)"
 set title "CPS vs SCR"
-plot "$Maintestfolder/b1ur1smscs1urh1msch1/SUMMARYSCR" using 2:xtic(1) title 'SCR' w l lc 0
-plot "$Maintestfolder/b1ur1smsc2surh1msch1/SUMMARYSCR" using 2:xtic(1) title 'SCR' w l lc 0
-plot "$Maintestfolder/b1ur2smsc1surh1msch1/SUMMARYSCR" using 2:xtic(1) title 'SCR' w l lc 0
-plot "$Maintestfolder/b2ur1smsc1surh1msch1/SUMMARYSCR" using 2:xtic(1) title 'SCR' w l lc 0
-plot "$Maintestfolder/b2ur1smsc2surh1msch1/SUMMARYSCR" using 2:xtic(1) title 'SCR' w l lc 0
-plot "$Maintestfolder/b2ur2smsc1surh1msch1/SUMMARYSCR" using 2:xtic(1) title 'SCR' w l lc 0
-plot "$Maintestfolder/b3ur1smsc1surh1msch1/SUMMARYSCR" using 2:xtic(1) title 'SCR' w l lc 0
+plot "$Maintestfolder/b1ur1smscs1urh1msch1/SUMMARYSCR" using 2:xtic(1) title 'b1ur1msc1' w l lc 0
+plot "$Maintestfolder/b1ur1smsc2surh1msch1/SUMMARYSCR" using 2:xtic(1) title 'b1ur1msc2' w l lc 1
+plot "$Maintestfolder/b1ur2smsc1surh1msch1/SUMMARYSCR" using 2:xtic(1) title 'b1ur2msc1' w l lc 2
+plot "$Maintestfolder/b2ur1smsc1surh1msch1/SUMMARYSCR" using 2:xtic(1) title 'b2ur1msc1' w l lc 3
+plot "$Maintestfolder/b2ur1smsc2surh1msch1/SUMMARYSCR" using 2:xtic(1) title 'b2ur1msc2' w l lc 4
+plot "$Maintestfolder/b2ur2smsc1surh1msch1/SUMMARYSCR" using 2:xtic(1) title 'b2ur2msc1' w l lc 5
+plot "$Maintestfolder/b3ur1smsc1surh1msch1/SUMMARYSCR" using 2:xtic(1) title 'b3ur1msc1' w l lc 6
 EOF
