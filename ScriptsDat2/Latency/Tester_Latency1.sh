640bc20554c31fc7686f6a5c28d8451f74e79417
juansorduz <GabrielOrozco>
2019-04-30 11:26:21 -0500

    Updating test scripts

--- ScriptsDat2/Latency/Tester_Latency1.sh
+++ ScriptsDat2/Latency/Tester_Latency1.sh
@@ -44,14 +44,16 @@ while [ "$stateTest" -eq '1' ]; do
   #echo $ERRORVARIABLE
   ERROR_LENGTH=${#ERRORVARIABLE}
   #echo $ERROR_LENGTH
-  if [ "$ERROR_LENGTH" -lt '300' ]
-  then
-    #echo Valor positivo $NumSipp $LATENCY
-    echo $LATENCY >> $testfolder/SingleLatencyTest$NumSipp.csv
 
-   #else
-     #echo Valor negativoo $NumSipp $LATENCY
-  fi
+  echo $LATENCY $ERROR_LENGTH >> $testfolder/SingleLatencyTest$NumSipp.csv
+  # if [ "$ERROR_LENGTH" -lt '300' ]
+  # then
+  #   #echo Valor positivo $NumSipp $LATENCY
+  #   echo $LATENCY >> $testfolder/SingleLatencyTest$NumSipp.csv
+  #
+  #  #else
+  #    #echo Valor negativoo $NumSipp $LATENCY
+  # fi
   source $testfolder/Variables.txt
   if [ "$stateTest" -eq '2' ]
   then
