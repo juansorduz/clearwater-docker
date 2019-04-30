71a3cc9168bf6d4b30cc81b876e46aed81d7977c
juansorduz <juanorduz@unicauca.edu.co>
2019-04-29 22:33:30 -0500

    Updating VM scripts

--- ScriptsDat3/Latency/Tester_Latency1.sh
+++ ScriptsDat3/Latency/Tester_Latency1.sh
@@ -36,8 +36,8 @@ source $testfolder/Variables.txt
 while [ "$stateTest" -eq '1' ]; do
 #echo REGISTRY
   START=$(date +%s.%N)
-  ERRORVARIABLE="$(docker exec $sipptest ./usr/share/clearwater/bin/sip-stress-latency$NumSipp 2>&1 > /dev/null)"
-  #ERRORVARIABLE="$(kubectl exec $sipptest ./usr/share/clearwater/bin/sip-stress-latency$NumSipp 2>&1)"
+  #ERRORVARIABLE="$(docker exec $sipptest ./usr/share/clearwater/bin/sip-stress-latency$NumSipp 2>&1 > /dev/null)"
+  ERRORVARIABLE="$(kubectl exec $sipptest ./usr/share/clearwater/bin/sip-stress-latency$NumSipp 2>&1)"
   #ERRORVARIABLE=0
   #kubectl exec $sipptest ./usr/share/clearwater/bin/sip-stress-latency$NumSipp
   END=$(date +%s.%N)
