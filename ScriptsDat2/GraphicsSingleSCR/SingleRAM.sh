83dc080139b996e2622961e11e21eb8c4af8f615
juansorduz <GabrielOrozco>
2019-05-06 10:02:42 -0500

    Updating Graph Scripts

---
+++ ScriptsDat2/GraphicsSingleSCR/SingleRAM.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+source ~/clearwater-docker/ScriptsDat2/TrafficGenerator/TestCharacteristics
+testMainfolder=$Maintestfolder/b${NumBono}urs${NumURS}mscs${NumMSCS}urh${NumURH}msch${NumMSCH}
+testfolder=$Maintestfolder/b${NumBono}urs${NumURS}mscs${NumMSCS}urh${NumURH}msch${NumMSCH}/$cps$duration
+Graphfolder=~/ClearwaterTestResults/Graphs
+mkdir -p $Graphfolder
+cd $Graphfolder
+#echo $Graphfolder
+
+gnuplot <<- EOF
+set terminal png size 800,500 enhanced font "Helvetica,20"
+red = "#FF0000"; green = "#00FF00"; blue = "#0000FF"; skyblue = "#87CEEB";
+set yrange [0:150]
+set style data histogram
+set style histogram cluster errorbars gap 1
+set style fill solid
+set boxwidth 0.9
+set xtics format ""
+set grid ytics
+set grid xtics
+set key outside below
+set terminal pdf
+set output "DespliegueVMCPSvsRAMDespliegue1.pdf"
+set xlabel "CPS"
+set ylabel "RAM (%)"
+set title "CPS vs RAM b${NumBono}urs${NumURS}mscs${NumMSCS}urh${NumURH}msch${NumMSCH}"
+plot "~/ClearwaterTestResults/Kubernetes5/b1urs1mscs1urh1msch1/SUMMARYVMRAM" using 18:19:xtic(1) title 'b1ur1msc1' lc 1, \
+     "~/ClearwaterTestResults/Kubernetes5/b2urs1mscs1urh1msch1/SUMMARYVMRAM" using 18:19:xtic(1) title 'b2ur1msc1' lc 2, \
+     "~/ClearwaterTestResults/Kubernetes5/b3urs1mscs1urh1msch1/SUMMARYVMRAM" using 18:19:xtic(1) title 'b3ur1msc1' lc 6, \
+     "~/ClearwaterTestResults/Kubernetes5/b3urs2mscs1urh1msch1/SUMMARYVMRAM" using 18:19:xtic(1) title 'b3ur2msc1' lc 7, \
+     "~/ClearwaterTestResults/Kubernetes5/b3urs1mscs2urh1msch1/SUMMARYVMRAM" using 18:19:xtic(1) title 'b3ur1msc2' lc 5, \
+     "~/ClearwaterTestResults/VM1/SUMMARYVMRAM" using 18:19:xtic(1) title 'VM' lc 8
+EOF
