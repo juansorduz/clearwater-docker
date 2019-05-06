c1ece3363a81f33540e47bd31cd409650e497d3f
juansorduz <GabrielOrozco>
2019-05-06 09:44:56 -0500

    Updating Graph Scripts

---
+++ ScriptsDat2/GraphicsSingleSCR/SingleCPU.sh
@@ -0,0 +1,24 @@
+gnuplot <<- EOF
+set terminal png size 800,500 enhanced font "Helvetica,20"
+red = "#FF0000"; green = "#00FF00"; blue = "#0000FF"; skyblue = "#87CEEB";
+set yrange [0:2000]
+set style data histogram
+set style histogram cluster errorbars gap 1
+set style fill solid
+set boxwidth 0.9
+set xtics format ""
+set grid ytics
+set grid xtics
+set key outside below
+set terminal pdf
+set output "DespliegueVMCPSvsCPU.pdf"
+set xlabel "CPS"
+set ylabel "CPU (%)"
+set title "CPS vs CPU b${NumBono}urs${NumURS}mscs${NumMSCS}urh${NumURH}msch${NumMSCH}"
+plot "~/ClearwaterTestResults/Summaries/b1urs1mscs1urh1msch1/SUMMARYVMCPU" using 18:19:xtic(1) title 'b1ur1msc1' lc 1, \
+     "~/ClearwaterTestResults/Summaries/b2urs1mscs1urh1msch1/SUMMARYVMCPU" using 18:19:xtic(1) title 'b2ur1msc1' lc 4, \
+     "~/ClearwaterTestResults/Summaries/b3urs1mscs1urh1msch1/SUMMARYVMCPU" using 18:19:xtic(1) title 'b3ur1msc1' lc 7, \
+     "~/ClearwaterTestResults/Summaries/b3urs2mscs1urh1msch1/SUMMARYVMCPU" using 18:19:xtic(1) title 'b3ur2msc1' lc 10, \
+     "~/ClearwaterTestResults/Summaries/b3urs1mscs2urh1msch1/SUMMARYVMCPU" using 18:19:xtic(1) title 'b3ur1msc2' lc 11, \
+     "~/ClearwaterTestResults/Summaries/VM/SUMMARYVMCPU" using 18:19:xtic(1) title 'VM' lc 8
+EOF
