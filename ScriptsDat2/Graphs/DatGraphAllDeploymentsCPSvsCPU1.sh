fc80ca95deb8b0bda0ab1d512e04b5195870bd84
juansorduz <juanorduz@unicauca.edu.co>
2019-05-09 15:24:48 -0500

    Updating Graph Scripts

--- ScriptsDat2/Graphs/DatGraphAllDeploymentsCPSvsCPU1.sh
+++ ScriptsDat2/Graphs/DatGraphAllDeploymentsCPSvsCPU1.sh
@@ -31,11 +31,20 @@ plot "~/ClearwaterTestResults/Summaries/b1urs1mscs1urh1msch1/SUMMARYVMCPU" using
      "~/ClearwaterTestResults/Summaries/b2urs1mscs2urh1msch1/SUMMARYVMCPU" using 18:19:xtic(1) title 'b2ur1msc2' lc 5, \
      "~/ClearwaterTestResults/Summaries/b2urs2mscs1urh1msch1/SUMMARYVMCPU" using 18:19:xtic(1) title 'b2ur2msc1' lc 6, \
      "~/ClearwaterTestResults/Summaries/b3urs1mscs1urh1msch1/SUMMARYVMCPU" using 18:19:xtic(1) title 'b3ur1msc1' lc 7, \
-     "~/ClearwaterTestResults/Summaries/b3urs2mscs1urh1msch1/SUMMARYVMCPU" using 18:19:xtic(1) title 'b3ur2msc1' lc 9, \
-     "~/ClearwaterTestResults/Summaries/b3urs1mscs2urh1msch1/SUMMARYVMCPU" using 18:19:xtic(1) title 'b3ur1msc2' lc 10, \
-     "~/ClearwaterTestResults/Summaries/b4urs1mscs1urh1msch1/SUMMARYVMCPU" using 18:19:xtic(1) title 'b4ur1msc1' lc 11, \
-     "~/ClearwaterTestResults/Summaries/b4urs2mscs1urh1msch1/SUMMARYVMCPU" using 18:19:xtic(1) title 'b4ur2msc1' lc 12, \
-     "~/ClearwaterTestResults/Summaries/b4urs1mscs2urh1msch1/SUMMARYVMCPU" using 18:19:xtic(1) title 'b4ur1msc2' lc 13, \
-     "~/ClearwaterTestResults/Summaries/b5urs1mscs1urh1msch1/SUMMARYVMCPU" using 18:19:xtic(1) title 'b5ur1msc1' lc 14, \
      "~/ClearwaterTestResults/Summaries/VM/SUMMARYVMCPU" using 18:19:xtic(1) title 'VM' lc 8
 EOF
+
+# "~/ClearwaterTestResults/Summaries/b1urs1mscs1urh1msch1/SUMMARYVMCPU" using 18:19:xtic(1) title 'b1ur1msc1' lc 1, \
+#      "~/ClearwaterTestResults/Summaries/b1urs1mscs2urh1msch1/SUMMARYVMCPU" using 18:19:xtic(1) title 'b1ur1msc2' lc 2, \
+#      "~/ClearwaterTestResults/Summaries/b1urs2mscs1urh1msch1/SUMMARYVMCPU" using 18:19:xtic(1) title 'b1ur2msc1' lc 3, \
+#      "~/ClearwaterTestResults/Summaries/b2urs1mscs1urh1msch1/SUMMARYVMCPU" using 18:19:xtic(1) title 'b2ur1msc1' lc 4, \
+#      "~/ClearwaterTestResults/Summaries/b2urs1mscs2urh1msch1/SUMMARYVMCPU" using 18:19:xtic(1) title 'b2ur1msc2' lc 5, \
+#      "~/ClearwaterTestResults/Summaries/b2urs2mscs1urh1msch1/SUMMARYVMCPU" using 18:19:xtic(1) title 'b2ur2msc1' lc 6, \
+#      "~/ClearwaterTestResults/Summaries/b3urs1mscs1urh1msch1/SUMMARYVMCPU" using 18:19:xtic(1) title 'b3ur1msc1' lc 7, \
+#      "~/ClearwaterTestResults/Summaries/b3urs2mscs1urh1msch1/SUMMARYVMCPU" using 18:19:xtic(1) title 'b3ur2msc1' lc 9, \
+#      "~/ClearwaterTestResults/Summaries/b3urs1mscs2urh1msch1/SUMMARYVMCPU" using 18:19:xtic(1) title 'b3ur1msc2' lc 10, \
+#      "~/ClearwaterTestResults/Summaries/b4urs1mscs1urh1msch1/SUMMARYVMCPU" using 18:19:xtic(1) title 'b4ur1msc1' lc 11, \
+#      "~/ClearwaterTestResults/Summaries/b4urs2mscs1urh1msch1/SUMMARYVMCPU" using 18:19:xtic(1) title 'b4ur2msc1' lc 12, \
+#      "~/ClearwaterTestResults/Summaries/b4urs1mscs2urh1msch1/SUMMARYVMCPU" using 18:19:xtic(1) title 'b4ur1msc2' lc 13, \
+#      "~/ClearwaterTestResults/Summaries/b5urs1mscs1urh1msch1/SUMMARYVMCPU" using 18:19:xtic(1) title 'b5ur1msc1' lc 14, \
+#      "~/ClearwaterTestResults/Summaries/VM/SUMMARYVMCPU" using 18:19:xtic(1) title 'VM' lc 8
