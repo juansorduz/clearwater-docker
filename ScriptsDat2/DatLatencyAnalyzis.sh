6422d14a8abb8cfaf0275b813614434b9d4ffdf8
juansorduz <juanorduz@unicauca.edu.co>
2019-05-13 10:19:24 -0500

    Latency scripts

---
+++ ScriptsDat2/DatLatencyAnalyzis.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+option=${1:-1}
+
+source ~/clearwater-docker/ScriptsDat2/TrafficGenerator/TestCharacteristics
+testfolderdeployment=$Maintestfolder/b${NumBono}urs${NumURS}mscs${NumMSCS}urh${NumURH}msch${NumMSCH}
+
+#Deleting old files
+[ -e $testfolder/SUMMARYLATENCYCDF ] && rm $testfolder/SUMMARYLATENCYCDF
+
+
+#exit 0
+cd $testfolderdeployment
+
+#SE GENERA UN ARCHIVO GENERAL CON TODA LA INFORMACION DEL DESPLIEGUE
+if [ "$option" -eq '1' ]
+then
+  cat */1/RecollectionLatency.csv > FullRecollectionLatency.csv
+fi
+if [ "$option" -eq '2' ]
+then
+  cat */1/RecollectionLatencycall-setup.csv  > FullRecollectionLatency.csv
+fi
+if [ "$option" -eq '3' ]
+then
+  cat */*/RecollectionLatency.csv > FullRecollectionLatency.csv
+fi
+if [ "$option" -eq '4' ]
+then
+  cat */*/RecollectionLatencycall-setup.csv > FullRecollectionLatency.csv
+fi
+
+#CHANGE FORMAT FILE
+sed -i 's/\./,/g' FullRecollectionLatency.csv
+
+sed -i 's/;/ /g' FullRecollectionLatency.csv
+
+awk '{$1=$3=""; print $0}' FullRecollectionLatency.csv > FullRecollectionLatency2.csv
+
+sort -k1,1n FullRecollectionLatency2.csv > SUMMARYLATENCYCDF
