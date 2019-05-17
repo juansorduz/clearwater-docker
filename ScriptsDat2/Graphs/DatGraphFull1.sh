22e69ece5581328743d0e1b9335a44444f3b8675
juansorduz <juanorduz@unicauca.edu.co>
2019-05-17 11:16:12 -0500

    Updating test scripts

---
+++ ScriptsDat2/Graphs/DatGraphFull1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+. ~/clearwater-docker/ScriptsDat2/Graphs/DatGraphAllDeploymentsCPSvsCPU2.sh
+. ~/clearwater-docker/ScriptsDat2/Graphs/DatGraphAllDeploymentsCPSvsRAM2.sh
+. ~/clearwater-docker/ScriptsDat2/Graphs/DatGraphAllDeploymentsCPSvsSCR2.sh
+. ~/clearwater-docker/ScriptsDat2/Graphs/DatGraphAllDeploymentsCPSvsSCRSC2.sh
+. ~/clearwater-docker/ScriptsDat2/Graphs/DatGraphAllDeploymentsLATENCYvsCDF.sh
