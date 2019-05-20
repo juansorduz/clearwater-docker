d9d4f5b19b5041be5912f2b52c355971a9ffdb59
juansorduz <juanorduz@unicauca.edu.co>
2019-05-20 11:06:15 -0500

    Updating test scripts

--- ScriptsDat2/DatStart1.sh
+++ ScriptsDat2/DatStart1.sh
@@ -75,6 +75,7 @@ cd
 cd clearwater-docker
 kubectl create configmap env-vars --from-literal=ZONE=default.svc.cluster.local
 kubectl apply -f kubernetes3
+exit 0
 sleep 300
 urcassandrapod=$(kubectl get pods | grep urcassandra | cut -d ' ' -f1)
 kubectl exec $urcassandrapod /usr/share/clearwater/crest-prov/src/metaswitch/crest/tools/stress_provision.sh $usuarios
